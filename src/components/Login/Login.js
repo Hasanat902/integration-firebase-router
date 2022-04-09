944e064cb0a3309c8bab963f8994422981fc9f8a
Hasanat902 <abulhasanat902@gmail.com>
2022-04-09 16:18:32 +0600

    google sign in added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {signInWithGoogle} = useFirebase();
+
     return (
         <div>
             <h2>Please LogIn</h2>
+            <div style={{margin: '20px'}}>
+                <button onClick={signInWithGoogle}>Google SignIn</button>
+            </div>
+
+            <form>
+                <input type="email" name="" id="" placeholder='Your Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Password' />
+                <br />
+                <input type="submit" value="LogIn" />
+            </form>
         </div>
     );
 };
